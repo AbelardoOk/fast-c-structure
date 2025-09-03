2acb67ab418f4fb56f397a084a4d3aac8d3dec04
AbelardoOk <abelardoemailprivado@gmail.com>
2025-09-03 17:34:55 -0400

    Feat: Snippet config

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const template = `#include <stdio.h>
+int main(){
+	printf("Hello World!\\n");
+	return 0;
+}`;
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log(
+    'Congratulations, your extension "fast-c-structure" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fast-c-structure" is now active!');
+  const disposable = vscode.commands.registerCommand(
+    "cSnippetStarter.createTemplate",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        editor.edit((editBuilder) => {
+          editBuilder.insert(new vscode.Position(0, 0), template);
+        });
+      }
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('fast-c-structure.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fast-c-structure!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
